b4a4626948894dfc659f6dd8f122d9af9921c5da
princz.1 <150322497+princz-1@users.noreply.github.com>
2023-11-12 10:12:00 +0530

    Multidimensional Arrays Basic Code
    Multidimensional Arrays 2D Arrays Basic Code

---
+++ MultidimensionalArrays.c
@@ -0,0 +1,16 @@
+//Multidimensional Arrays 2D Arrays
+
+#include <stdio.h>
+//2D Arrays
+int main(void) {
+  int arr[][] = {{1,2},{3,4}};//Declare
+
+  //Access
+  arr[0][0];
+  arr[0][1];
+  arr[1][0];
+  arr[1][1];
+  
+  
+  return 0;
+}
